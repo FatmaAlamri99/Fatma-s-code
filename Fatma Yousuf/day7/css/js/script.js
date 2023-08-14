2edacb72bbc023f662bafe0494f4c625c08b3f91
FatmaAlamri99 <141591644+FatmaAlamri99@users.noreply.github.com>
2023-08-15 02:03:06 +0400

    Add files via upload

---
+++ Fatma Yousuf/day7/css/js/script.js
@@ -0,0 +1,55 @@
+// let btn=document.getElementsByClassName('btn')
+// btn.addEventListener('click',function(){
+// let inp1=document.getElementById('inp1')
+// let inp2=document.getElementById('inp2')
+// let result=parseInt(inp1.value) +parseInt(inp2.value);
+// text.innerHTML=result
+
+// console.log(result)
+
+
+
+
+
+
+// })
+
+
+// console.log(text)
+// let btn=document.getElementsByClassName('btn')
+// btn.addEventListener('click',function(){
+//     let box=document.getElementsByClassName('box')[0]
+
+//     box.classList.toggle('x')
+
+
+
+
+
+
+// })
+
+// let box=document.getElementsByClassName('box')[0]
+// window.addEventListener('keydown',function(arow){
+// let box=this.document.getElementsByName('box')[0]
+//     if(arow.code==="KeyF"){
+// console.log(arow.code)
+
+// document.body.style.background="blue";
+//  document.body.style.margin=left;
+//     }
+
+
+
+
+// })
+
+setTimeout(function(){
+
+   
+    
+
+   console.log("helllllllllo")
+
+
+},3000)
---
+++ Fatma Yousuf/day7/js/script.js
@@ -0,0 +1,18 @@
+let btn=document.getElementsByClassName('btn')
+btn.addEventListener('click',function(){
+let inp1=document.getElementById('inp1')
+let inp2=document.getElementById('inp2')
+let result=inp1.value +inp2.value;
+text.innerHTML=result
+
+console.log(result)
+
+
+
+
+
+
+})
+
+
+console.log(text)
