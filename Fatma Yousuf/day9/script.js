8c898ff9ca93c361aae98069050f93fe5e8bf2a6
FatmaAlamri99 <141591644+FatmaAlamri99@users.noreply.github.com>
2023-08-17 01:23:16 +0400

    Add files via upload

---
+++ Fatma Yousuf/day9/script.js
@@ -0,0 +1,29 @@
+// let element =document.createElement('div')
+// element.setAttribute('id','box')
+// element.classList.add('box')
+// let e2 =document.createElement('h1')
+
+// document.write('<h1> hello </h1>')
+
+
+
+
+// element.appendChild('h1')
+// document.body.appendChild(element)
+
+let pName=document.getElementById('pName')
+let pPrice=document.getElementById('pPrice')
+let pcatogory=document.getElementById('pcategory')
+let pdescription=document.getElementById('pdescription')
+let btn=document.getElementById('add')
+btn=[]
+btn.addEventListener('click',(){
+
+    pName:pro
+
+
+
+
+
+
+})
